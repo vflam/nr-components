92a713d6ee903b86bfe57a248d792bb1f1d3cb3e
Vincent Massias <vi.massias@gmail.com>
2024-07-27 17:20:49 +0200

    changed dateformat to use utc in date input

--- DateInput.vue
+++ DateInput.vue
@@ -15,15 +15,27 @@ export default {
   created() {
     let val = this.modelValue;
     if (val == null) {
-      this.dateString = dateFormat(new Date());
+      this.dateString = this.dateFormat(new Date());
     } else {
-      this.dateString = dateFormat(this.modelValue);
+      this.dateString = this.dateFormat(this.modelValue);
     }
   },
 
   methods: {
     change() {
       this.$emit("update:modelValue", new Date(this.dateString));
+    },
+
+    dateFormat(date) {
+      if (typeof date === "string") {
+        date = new Date(date);
+      }
+
+      const year = date.getUTCFullYear().toString().padStart(4, "0");
+      const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+      const day = date.getUTCDate().toString().padStart(2, "0");
+
+      return `${year}-${month}-${day}`;
     },
   },
 };
