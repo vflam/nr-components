a443c7cc78e965aa78ac35feced9957a69cee4a4
Vincent Massias <vi.massias@gmail.com>
2024-08-12 09:11:13 +0200

    adjusted padding for no close popups

--- PopupDialog.vue
+++ PopupDialog.vue
@@ -219,8 +219,8 @@ export default {
 html.dark .box {
   scrollbar-color: rgba(255, 255, 255, 0.3) rgba(0, 0, 0, 0);
 }
-.content {
-  padding-bottom: 10px;
+.close-wrap {
+  margin-top: 10px;
 }
 
 .allowoverflow .content {
