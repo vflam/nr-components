3b38d6cfce8608e4bdd1b8a3950f8feec71d17d9
Vincent Massias <vi.massias@gmail.com>
2025-02-24 15:16:17 +0100

    removed overflow none

--- PopupDialog.vue
+++ PopupDialog.vue
@@ -266,7 +266,6 @@ export default {
   overflow-y: auto;
   scrollbar-width: thin;
   max-width: v-bind(width);
-  overflow: visible;
 }
 html.dark .box {
   scrollbar-color: rgba(255, 255, 255, 0.3) rgba(0, 0, 0, 0);
