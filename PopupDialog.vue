d562c887ebef056dcd138903de766d5e7afcfe5f
Vincent Massias <vi.massias@gmail.com>
2025-02-24 15:33:39 +0100

    fixed overflow visible

--- PopupDialog.vue
+++ PopupDialog.vue
@@ -266,6 +266,7 @@ export default {
   overflow-y: auto;
   scrollbar-width: thin;
   max-width: v-bind(width);
+  overflow: auto;
 }
 html.dark .box {
   scrollbar-color: rgba(255, 255, 255, 0.3) rgba(0, 0, 0, 0);
